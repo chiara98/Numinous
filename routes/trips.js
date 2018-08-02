bf0838493faadf794862a7295761ffbc8a63e117
Chirara Lin <chiara98tx@yahoo.com>
2018-08-02 15:29:14 -0700

    removed html element

--- routes/trips.js
+++ routes/trips.js
@@ -7,22 +7,6 @@ const auth = require('./helpers/auth');
 
 // Trips index
 router.get('/', (req,res, next) => {
-  // console.log("trying to go to main");
-  // console.log(req.query.city1);
-  // console.log(req.query.date1);
-  // console.log(req.query.date2);
-
-  var date1 = new Date(req.query.date1);
-  var date2 = new Date(req.query.date2);
-  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-  var diffDays = Math.round(timeDiff/ (1000 * 3600 *24));
-  console.log(diffDays);
-  //
-  // $date1 = new Date(req.query.date1);
-  // $date2 = new Date(req.query.date2);
-
-
-
 
   res.render("trips/index", {layout: 'tripsLayout.hbs', city1: req.query.city1, city2: req.query.city2, city3: req.query.city3,
   city4: req.query.city4, city5: req.query.city5, date1: req.query.date1, date2: req.query.date2});
