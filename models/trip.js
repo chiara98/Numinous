344b7fdbccb37a5c911645a56fef0af36f18dd18
Chirara Lin <chiara98tx@yahoo.com>
2018-08-07 09:56:04 -0700

    Added more styling

--- models/trip.js
+++ models/trip.js
@@ -3,15 +3,13 @@ const Schema = mongoose.Schema;
 
 
 const TripSchema = new Schema({
-  starting_Date: {type: Date, required: true},
-  ending_Date: {type: Date, required: true},
+   starting_Date: {type: Date, required: true},
+   ending_Date: {type: Date, required: true},
   starting_city: {type: String, required: true},
   ending_city: {type: String, required: true},
   middle_city1: {type: String, required: false},
   middle_city2: {type: String, required: false},
   middle_city3: {type: String, required: false},
-
-
-});
+ });
 
 module.exports = mongoose.model('Trip', TripSchema);
